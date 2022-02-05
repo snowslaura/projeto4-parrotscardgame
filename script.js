b25dc96d9be52186e5c01f8fe01e734e998b3d71
Laura Neves <lauracmneves@gmail.com>
2022-02-05 20:59:27 -0300

    Versao com virada de cartas funcionando

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Iniciando do jogo 
+
 let numeroDeCartasDoJogo = prompt('Qual o número de cartas?');
 
 while (numeroDeCartasDoJogo % 2 !== 0 || numeroDeCartasDoJogo < 4 || numeroDeCartasDoJogo > 14){
@@ -5,6 +7,8 @@ while (numeroDeCartasDoJogo % 2 !== 0 || numeroDeCartasDoJogo < 4 || numeroDeCar
 }
 
 let numeroDeCartas = parseInt((numeroDeCartasDoJogo)/2);
+
+// Lógica para embaralhar as cartas 
 
 let arrayDeIndices = [0,1,2,3,4,5,6];
 
@@ -20,6 +24,8 @@ function comparador() {
 
 let cartas = [];
 
+// Lógica para selecionar as cartas a serem jogadas
+
 function selecionarCartas (){
 
     for (let i=0; i< numeroDeCartas; i++){
@@ -32,17 +38,20 @@ selecionarCartas();
 
 embaralhaCartas(cartas);
 
+// Lógica para mostrar as cartas
+
 function mostrarCartas(){
 
-    let conteudoDeCartas = document.querySelector(".content");
+    let conteudoDeCartas = document.querySelector(".conteudo");
+
      for(let i=0; i< cartas.length ; i++){     
         conteudoDeCartas.innerHTML += `
-            <div class="carta" onclick="virarCarta(this)">
-                <span class="frente">
+            <div class="carta" onclick="virarCarta(this)" >
+                <span class="frente"  >
                     <img src="./img/parrot.${cartas[i]}.gif"></img>
                 </span>
                 
-                <span class="verso" >
+                <span class="verso"  >
                     <img src="./img/front.png "></img>
                 </span>            
             </div>    
@@ -52,17 +61,48 @@ function mostrarCartas(){
 
 mostrarCartas();
 
+// Logica para virar as cartas
+
+let cartaVirada = false;
+
 function virarCarta(carta){
+
     let cartaFrente = carta.querySelector(".frente");
-    cartaFrente.classList.toggle("virada");
+    cartaFrente.classList.add("virada");
 
     let cartaVerso = carta.querySelector(".verso");
-    cartaVerso.classList.toggle("virada");
+    cartaVerso.classList.add("virada");
+
+    if (!cartaVirada){
+        
+        cartaVirada = true;
+        primeiraCarta = carta;
+    }
+    else {
+        cartaVirada = false;
+        segundaCarta = carta;
+   
+        comparandoCartas(primeiraCarta,segundaCarta);
+    }
 }
 
+// Lógica para comparar as cartas 
 
+function comparandoCartas(carta1,carta2){
+    if (carta1.innerHTML!=carta2.innerHTML){
 
+        setTimeout(function(){ 
+            let cartaFrente1 = carta1.querySelector(".frente");
+            cartaFrente1.classList.remove("virada");
 
+            let cartaVerso1 = carta1.querySelector(".verso");
+            cartaVerso1.classList.remove("virada");
 
+            let cartaFrente2 = carta2.querySelector(".frente");
+            cartaFrente2.classList.remove("virada");
 
-
+            let cartaVerso2 = carta2.querySelector(".verso");
+            cartaVerso2.classList.remove("virada");
+        }, 1000)
+    }
+}
